93b9dda6deda3436ec81d604f58052059d30713a
Gabriel Mota <gabriel.mota@gpnet.com.br>
2022-09-13 18:32:57 -0300

    Implementação do Recoil

--- src/components/Calendario/index.tsx
+++ src/components/Calendario/index.tsx
@@ -1,36 +1,62 @@
-
-import React from 'react'
-import { IEvento } from '../../interfaces/IEvento';
-import style from './Calendario.module.scss';
-import ptBR from './localizacao/ptBR.json'
-import Kalend, { CalendarView } from 'kalend'
-import 'kalend/dist/styles/index.css';
+import React from "react";
+import style from "./Calendario.module.scss";
+import ptBR from "./localizacao/ptBR.json";
+import Kalend, { CalendarEvent, CalendarView, OnEventDragFinish } from "kalend";
+import "kalend/dist/styles/index.css";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { listaDeEventosState } from "../../state/atom";
+import { IEvento } from "../../interfaces/IEvento";
+import useAtualizarEvento from "../../state/hooks/useAtualizarEvento";
 
 interface IKalendEvento {
-  id?: number
-  startAt: string
-  endAt: string
-  summary: string
-  color: string
+  id?: number;
+  startAt: string;
+  endAt: string;
+  summary: string;
+  color: string;
 }
 
-const Calendario: React.FC<{ eventos: IEvento[] }> = ({ eventos }) => {
+const Calendario: React.FC = () => {
+  const eventos = useRecoilValue(listaDeEventosState);
 
   const eventosKalend = new Map<string, IKalendEvento[]>();
 
-  eventos.forEach(evento => {
-    const chave = evento.inicio.toISOString().slice(0, 10)
+  const setListaDeEventos = useSetRecoilState<IEvento[]>(listaDeEventosState);
+
+  const atualizaEvento = useAtualizarEvento();
+
+  eventos.forEach((evento) => {
+    const chave = evento.inicio.toISOString().slice(0, 10);
     if (!eventosKalend.has(chave)) {
-      eventosKalend.set(chave, [])
+      eventosKalend.set(chave, []);
     }
     eventosKalend.get(chave)?.push({
       id: evento.id,
       startAt: evento.inicio.toISOString(),
       endAt: evento.fim.toISOString(),
       summary: evento.descricao,
-      color: 'blue'
-    })
-  })
+      color: "blue",
+    });
+  });
+
+  const onEventDragFinish: OnEventDragFinish = (
+    kalendEventoInalterado: CalendarEvent,
+    kalendEventoAtualizado: CalendarEvent
+  ) => {
+    const evento = eventos.find(
+      (evento) => evento.descricao === kalendEventoAtualizado.summary
+    );
+    if (evento) {
+      const eventoAtualizado = {
+        ...evento,
+      };
+      eventoAtualizado.inicio = new Date(kalendEventoAtualizado.startAt);
+      eventoAtualizado.fim = new Date(kalendEventoAtualizado.endAt);
+
+      atualizaEvento(eventoAtualizado);
+    }
+  };
+
   return (
     <div className={style.Container}>
       <Kalend
@@ -38,14 +64,15 @@ const Calendario: React.FC<{ eventos: IEvento[] }> = ({ eventos }) => {
         initialDate={new Date().toISOString()}
         hourHeight={60}
         initialView={CalendarView.WEEK}
-        timeFormat={'24'}
-        weekDayStart={'Monday'}
-        calendarIDsHidden={['work']}
-        language={'customLanguage'}
+        timeFormat={"24"}
+        weekDayStart={"Monday"}
+        calendarIDsHidden={["work"]}
+        language={"customLanguage"}
         customLanguage={ptBR}
+        onEventDragFinish={onEventDragFinish}
       />
     </div>
   );
-}
+};
 
-export default Calendario
+export default Calendario;
