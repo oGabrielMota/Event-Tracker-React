fbbbdc9961b55ed691398c8625e1da779a552134
Gabriel Mota <gabriel.mota@gpnet.com.br>
2022-09-14 10:56:05 -0300

    Colocando Debug e Finalzindo Projeto

--- src/state/atom.ts
+++ src/state/atom.ts
@@ -1,10 +1,11 @@
 import { atom } from "recoil";
 import { IEvento } from "../interfaces/IEvento";
 import { IFiltrosDeEventos } from "../interfaces/IFiltrosDeEventos";
+import { eventosASync } from "./seletores";
 
 export const listaDeEventosState = atom<IEvento[]>({
   key: "listaDeEventosState",
-  default: [],
+  default: eventosASync,
 });
 
 export const filtroDeEventos = atom<IFiltrosDeEventos>({
